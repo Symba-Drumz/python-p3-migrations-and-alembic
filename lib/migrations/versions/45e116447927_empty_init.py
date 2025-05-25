c0d7710edb49bda23e42333ab17e87bbc385c067
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-25 12:15:46 +0300

    Set up Alembic migrations for Student model

---
+++ lib/migrations/versions/45e116447927_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 45e116447927
+Revises: 
+Create Date: 2025-05-25 11:55:07.176266
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '45e116447927'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
